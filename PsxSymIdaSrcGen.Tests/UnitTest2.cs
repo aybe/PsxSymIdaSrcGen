8fa1e4feb73b207eb9b7d1c2205bc20787a8f8bd
aybe <aybe@users.noreply.github.com>
2023-11-25 09:34:38 +0100

    cleanup

--- PsxSymIdaSrcGen.Tests/UnitTest2.cs
+++ PsxSymIdaSrcGen.Tests/UnitTest2.cs
@@ -5,87 +5,24 @@ using System.Text.RegularExpressions;
 namespace PsxSymIdaSrcGen.Tests;
 
 [TestClass]
-public partial class UnitTest2
+public class UnitTest2
 {
     [TestMethod]
     public void TestMethod1()
     {
         const string path = @"C:\Temp\SLES_001.15.c";
 
-        var info = new Source(path);
+        var source = new Source(path);
 
-        // declarations
+        var declarations = source.GetDeclarations();
+        var variables = source.GetVariables();
+        var functions = source.GetFunctions();
 
-        var declarations = info.GetDeclarations();
+        Console.WriteLine($"Declaration count: {declarations.Count}");
+        Console.WriteLine($"Variable block length: {variables.Length}");
+        Console.WriteLine($"Function count: {functions.Count}");
 
-        Console.WriteLine($"Declarations count: {declarations.Count}");
-
-        // variables
-
-        var variables = info.GetVariables();
-
-        Console.WriteLine($"Variables block length: {variables.Length}");
-
-        // implementations
-
-        var start = info.LineFirstImplementation;
-        var lines = new List<int>();
-
-        foreach (var line in info.TextImplementations)
-        {
-            if (Source.RegexFunctionAddress().Match(line) is { Success: true })
-            {
-                lines.Add(start);
-            }
-
-            start++;
-        }
-
-        lines.Add(info.LineEndOfFile);
-
-        var ranges = new List<Range>();
-
-        for (var i = 0; i < lines.Count - 1; i++)
-        {
-            ranges.Add(new Range(lines[i], lines[i + 1]));
-        }
-
-        var functions = ranges.Select(s => info.Text.AsSpan(s).ToArray()).ToList();
-
-        var sourceFunctions = new List<SourceFunction>(functions.Count);
-
-        foreach (var functionText in functions)
-        {
-            var functionFile = GetMatchGroupValue(functionText, 1, RegexFunctionFile());
-
-            var functionName = GetMatchGroupValue(functionText, 1, RegexFunctionName());
-
-            functionFile ??= string.Empty;
-
-            functionName ??= GetMatchGroupValue(functionText, 1, Source.RegexFunctionAddress());
-
-            if (functionName == null)
-            {
-                throw new InvalidOperationException();
-            }
-
-            var sourceFunction = new SourceFunction(functionFile, functionName, functionText);
-
-            sourceFunctions.Add(sourceFunction);
-        }
-
-        var mainFile = sourceFunctions
-            .FirstOrDefault(s => string.Equals(Path.GetFileName(s.File), "MAIN.C", StringComparison.OrdinalIgnoreCase))?
-            .File;
-
-        mainFile ??= @"C:\MAIN.C";
-
-        foreach (var function in sourceFunctions.Where(s => string.IsNullOrEmpty(s.File)))
-        {
-            function.File = mainFile;
-        }
-
-        var lookup = sourceFunctions.ToLookup(s => s.File);
+        var lookup = functions.ToLookup(s => s.File);
 
         foreach (var grouping in lookup)
         {
@@ -96,6 +33,75 @@ public partial class UnitTest2
             }
         }
     }
+}
+
+public sealed partial class Source
+{
+    public Source(string path)
+    {
+        Text = File.ReadAllLines(path);
+
+        LineFirstDeclaration =
+            Array.FindIndex(Text, RegexDeclaration1().IsMatch);
+
+        LineFirstVariable =
+            Array.FindIndex(Text, RegexVariable1().IsMatch);
+
+        LineFirstFunction =
+            Array.FindIndex(Text, RegexFunctionAddress().IsMatch);
+
+        LineEndOfFile =
+            Array.FindIndex(Text, RegexFunctionBlockEnd().IsMatch);
+
+        TextDeclarations =
+            Text.AsSpan(new Range(LineFirstDeclaration, LineFirstVariable)).ToArray();
+
+        TextVariables =
+            Text.AsSpan(new Range(LineFirstVariable, LineFirstFunction)).ToArray();
+
+        TextFunctions =
+            Text.AsSpan(new Range(LineFirstFunction, LineEndOfFile)).ToArray();
+    }
+
+    private int LineFirstDeclaration { get; }
+
+    private int LineFirstVariable { get; }
+
+    private int LineFirstFunction { get; }
+
+    private int LineEndOfFile { get; }
+
+    private string[] Text { get; }
+
+    private string[] TextDeclarations { get; }
+
+    private string[] TextFunctions { get; }
+
+    private string[] TextVariables { get; }
+
+    [GeneratedRegex(@"^//\sFunction\sdeclarations$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    private static partial Regex RegexDeclaration1();
+
+    [GeneratedRegex(@"^(?![\s/]+).*?(\w+)\(.*\);", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    private static partial Regex RegexDeclarationName();
+
+    [GeneratedRegex(@"^/{2}-{5}\s\(([A-Z0-9]{8})\)\s-{56}$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    private static partial Regex RegexFunctionAddress();
+
+    [GeneratedRegex(@"^/{2}\s\[PSX-MND-SYM\]\sFunction\sfile\s=\s(\S+)$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    private static partial Regex RegexFunctionFile();
+
+    [GeneratedRegex(@"^/{2}\s\[PSX-MND-SYM\]\sFunction\sname\s=\s(\w+)$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    private static partial Regex RegexFunctionName();
+
+    [GeneratedRegex(@"^/{2}\snfuncs=\d+", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    private static partial Regex RegexFunctionBlockEnd();
+
+    [GeneratedRegex(@"^//\sData\sdeclarations$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    private static partial Regex RegexVariable1();
+
+    [GeneratedRegex(@"^(?!\s+)(?:\w+\s)+\(?(?:__fastcall\s)?\**(\w+)", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
+    private static partial Regex RegexVariableName();
 
     private static string? GetMatchGroupValue(IEnumerable<string> lines, int group, Regex regex)
     {
@@ -123,76 +129,6 @@ public partial class UnitTest2
         return null;
     }
 
-    [GeneratedRegex(@"^/{2}\s\[PSX-MND-SYM\]\sFunction\sname\s=\s(\w+)$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-    private static partial Regex RegexFunctionName();
-
-    [GeneratedRegex(@"^/{2}\s\[PSX-MND-SYM\]\sFunction\sfile\s=\s(\S+)$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-    private static partial Regex RegexFunctionFile();
-
-
-    [GeneratedRegex(@"^(?!\s+)(?:\w+\s)+\(?(?:__fastcall\s)?\**(\w+)", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-    private static partial Regex RegexVariableName();
-}
-
-public sealed partial class Source
-{
-    public Source(string path)
-    {
-        Text = File.ReadAllLines(path);
-
-        LineFirstDeclaration =
-            Array.FindIndex(Text, RegexFunctionDeclarations().IsMatch);
-
-        LineFirstVariable =
-            Array.FindIndex(Text, RegexDataDeclarations().IsMatch);
-
-        LineFirstImplementation =
-            Array.FindIndex(Text, RegexFunctionAddress().IsMatch);
-
-        LineEndOfFile =
-            Array.FindIndex(Text, RegexEndOfFile().IsMatch);
-
-        TextDeclarations =
-            Text.AsSpan(new Range(LineFirstDeclaration, LineFirstVariable)).ToArray();
-
-        TextVariables =
-            Text.AsSpan(new Range(LineFirstVariable, LineFirstImplementation)).ToArray();
-
-        TextImplementations =
-            Text.AsSpan(new Range(LineFirstImplementation, LineEndOfFile)).ToArray();
-    }
-
-    private int LineFirstDeclaration { get; }
-
-    private int LineFirstVariable { get; }
-
-    public int LineFirstImplementation { get; }
-
-    public int LineEndOfFile { get; }
-
-    public string[] Text { get; }
-
-    private string[] TextDeclarations { get; }
-
-    public string[] TextImplementations { get; }
-
-    private string[] TextVariables { get; }
-
-    [GeneratedRegex(@"^/{2}-{5}\s\(([A-Z0-9]{8})\)\s-{56}$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-    public static partial Regex RegexFunctionAddress();
-
-    [GeneratedRegex(@"^//\sFunction\sdeclarations$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-    private static partial Regex RegexFunctionDeclarations();
-
-    [GeneratedRegex(@"^//\sData\sdeclarations$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-    private static partial Regex RegexDataDeclarations();
-
-    [GeneratedRegex(@"^/{2}\snfuncs=\d+", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-    private static partial Regex RegexEndOfFile();
-
-    [GeneratedRegex(@"^(?![\s/]+).*?(\w+)\(.*\);", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-    private static partial Regex RegexDeclarationName();
-
     public Dictionary<string, string> GetDeclarations()
     {
         var declarations = new Dictionary<string, string>();
@@ -213,6 +149,68 @@ public sealed partial class Source
         var variables = TextVariables.ToArray();
 
         return variables;
+    }
+
+    public List<SourceFunction> GetFunctions()
+    {
+        var start = LineFirstFunction;
+        var lines = new List<int>();
+
+        foreach (var line in TextFunctions)
+        {
+            if (RegexFunctionAddress().Match(line) is { Success: true })
+            {
+                lines.Add(start);
+            }
+
+            start++;
+        }
+
+        lines.Add(LineEndOfFile);
+
+        var ranges = new List<Range>();
+
+        for (var i = 0; i < lines.Count - 1; i++)
+        {
+            ranges.Add(new Range(lines[i], lines[i + 1]));
+        }
+
+        var functions = ranges.Select(s => Text.AsSpan(s).ToArray()).ToList();
+
+        var sourceFunctions = new List<SourceFunction>(functions.Count);
+
+        foreach (var functionText in functions)
+        {
+            var functionFile = GetMatchGroupValue(functionText, 1, RegexFunctionFile());
+
+            var functionName = GetMatchGroupValue(functionText, 1, RegexFunctionName());
+
+            functionFile ??= string.Empty;
+
+            functionName ??= GetMatchGroupValue(functionText, 1, RegexFunctionAddress());
+
+            if (functionName == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var sourceFunction = new SourceFunction(functionFile, functionName, functionText);
+
+            sourceFunctions.Add(sourceFunction);
+        }
+
+        var mainFile = sourceFunctions
+            .FirstOrDefault(s => string.Equals(Path.GetFileName(s.File), "MAIN.C", StringComparison.OrdinalIgnoreCase))?
+            .File;
+
+        mainFile ??= @"C:\MAIN.C";
+
+        foreach (var function in sourceFunctions.Where(s => string.IsNullOrEmpty(s.File)))
+        {
+            function.File = mainFile;
+        }
+
+        return sourceFunctions;
     }
 }
 
